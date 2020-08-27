8d65ca5dc058a617b5d8354fb21ecd7f3d738ce7
Heymi <heymicodes@gmail.com>
2020-08-27 14:57:31 +0200

    Create feed() method to retrieve lastest tweet
    Once the like feature is added, the method will only return latest tweet from the people followed by the user

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function feed()
+    {
+        // TO DO : only request logs from followed user
+        return Log::latest()->get();
+    }
 }
