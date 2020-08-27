bbf938c778dc553686fcd226b2409c6234e5f59b
Heymi <heymicodes@gmail.com>
2020-08-27 15:50:09 +0200

    Create form to publish Log
    The feature doesn't allow the timeline selection yet.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,25 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            {{-- Log Creation form --}}
+            <div class="card">
+                <form method="POST" action="/logs">
+                    @csrf
+                    <div class="from-group">
+                        <div class="card-header"><label for="logBody">{{ __('Add a log') }}</label></div>
+                        <div class="card-body">
+                            <textarea class="form-control" name="body" id="logBody" rows="3" placeholder="Share your story!"></textarea>
+                            <hr>
+                            <button type="submit" class ="btn btn-secondary">Log my day!</button>
+                        </div>
+                        @error('body')
+                            <div class="bg-danger text-white">{{ __('Sorry, you can\'t publish an empty log!')}}</div>
+                        @enderror
+                    </div>
+                </form>
+            </div>
+
+            {{-- Logs Feed --}}
             <div class="card">
                 <div class="card-header">{{ __('Home') }}</div>
 
