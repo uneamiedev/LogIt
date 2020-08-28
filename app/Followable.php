04354627f91b9b8e3511ff319eeb3823c7c99bd8
Heymi <heymicodes@gmail.com>
2020-08-28 22:24:49 +0200

    Create method to check if logged user follows given user

--- app/Followable.php
+++ app/Followable.php
@@ -21,4 +21,14 @@ trait Followable
     {
         return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
     }
+
+    /**
+     * Check if logged user is following given user
+     */
+    public function isFollowing(User $user)
+    {
+        return $this->follows()
+            ->where('following_user_id', $user->id)
+            ->exists();
+    }
 }
