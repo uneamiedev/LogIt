a14c99257b4f91dc3ee8b4dd6ad9243f0607a39e
Heymi <heymicodes@gmail.com>
2020-08-28 16:01:34 +0200

    Add timeline select field in Log's creation form
    Only show the log's creation form when user has at least 1 timeline.
    
    TODO : Add timeline's creation form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,42 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            {{-- Log Creation form --}}
-            <div class="card">
-                <form method="POST" action="/logs">
-                    @csrf
-                    <div class="from-group">
-                        <div class="card-header"><label for="logBody">{{ __('Add a log') }}</label></div>
-                        <div class="card-body">
-                            <textarea class="form-control" name="body" id="logBody" rows="3" placeholder="Share your story!"></textarea>
-                            <hr>
-                            <button type="submit" class ="btn btn-secondary">Log my day!</button>
+            {{-- TO DO: Add timeline creation form --}}
+
+            @if($timelines->count() > 0)
+                {{-- Log Creation form --}}
+                <div class="card">
+                    <form method="POST" action="/logs">
+                        @csrf
+                        <div class="from-group">
+                            <div class="card-header">{{ __('Add a log') }}</div>
+
+                            <div class="card-body">
+                                <label for="timelineSelect">{{ __('Select a project') }}</label>
+                                <select name="timeline" id="timelineSelect" class="custom-select">
+                                    @foreach($timelines as $timeline)
+                                        @dump($timeline)
+                                        <option value="{{ $timeline->id }}">{{ $timeline->title }}</option>
+                                    @endforeach
+                                </select>
+                                @error('timeline')
+                                    <div class="bg-danger text-white">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="card-body">
+                                <label for="logBody" class="sr-only">{{ __('How is it going ?') }}</label>
+                                <textarea class="form-control" name="body" id="logBody" rows="3" placeholder="Share your story!"></textarea>
+                                <hr>
+                                <button type="submit" class ="btn btn-secondary">Log my day!</button>
+                                @error('body')
+                                    <div class="bg-danger text-white">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
-                        @error('body')
-                            <div class="bg-danger text-white">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
+            @endif
 
             {{-- Logs Feed --}}
             <div class="card">
