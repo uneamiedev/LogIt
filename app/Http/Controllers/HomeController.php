d2edb7879ab676171f7a2ae8cdc03d367cf107ab
Heymi <heymicodes@gmail.com>
2020-08-28 15:05:13 +0200

    Send User's timelines to the home view
    This data is use to generate the select in the Log creation form.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,11 +21,14 @@ class HomeController extends Controller
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
+     *
+     * timelines contains user's timeline from latest to oldest
      */
     public function index()
     {
         return view('home', [
             'logs' => auth()->user()->feed(),
+            'timelines' => auth()->user()->timelines()->latest()->get(),
         ]);
     }
 }
