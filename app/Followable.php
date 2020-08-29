24cedec2444bdd184d07a8c9be5219e702263c63
Heymi <heymicodes@gmail.com>
2020-08-29 20:11:45 +0200

    Fix followers() method rerun value
    The method use to return the following instead of the followers.
    Because the key where passed in the wrong order.

--- app/Followable.php
+++ app/Followable.php
@@ -51,7 +51,7 @@ trait Followable
      */
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+        return $this->belongsToMany(User::class, 'follows', 'following_user_id', 'user_id');
     }
 
     /**
