15c9e44fdb5df14385c9cd035e648de583b8cd93
Heymi <heymicodes@gmail.com>
2020-08-31 01:06:30 +0200

    Use Likable trait in Timeline model

--- app/Timeline.php
+++ app/Timeline.php
@@ -4,9 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use App\Likable;
 
 class Timeline extends Model
 {
+    use Likable;
+
     protected $guarded = [];
 
     public function user()
