2d72ade9160caf37c13a98fa110f007c804919d9
Heymi <heymicodes@gmail.com>
2020-09-25 21:34:17 +0200

    Create footer component

---
+++ resources/views/common/footer.blade.php
@@ -0,0 +1,29 @@
+<footer>
+    <div>
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('images/svg/logit.svg') }}" alt="LogIt logo">
+        </a>
+        <p>Behind every project hides an adventure</p>
+    </div>
+    <div>
+        <div>
+            Exemples
+        </div>
+        <ul>
+            <li><a href="#">LogIt</a></li>
+            <li><a href="#">Sapiens</a></li>
+            <li><a href="#">30daysOfCode</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <div>
+            Exemples
+        </div>
+        <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+</footer>
