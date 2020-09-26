7ede6efca3c75c06d23f27fa66a5e698b973ad2c
Heymi <heymicodes@gmail.com>
2020-09-26 18:17:19 +0200

    Replace bootstrap by custom css
    Only update logs files shown on timelines

--- resources/views/logs/custom.blade.php
+++ resources/views/logs/custom.blade.php
@@ -1,13 +1,9 @@
-<div class="card mt-4 log log--custom mb-4">
-    <div class="card-body">
-        <p>{{ $log->body }}</p>
-        @can('delete', $log)
-        <form method="POST" action="{{ route('log.destroy', $log) }}">
-            @csrf
-            @method('DELETE')
-            <button class="btn btn-outline-danger" type="submit">{{ __('Delete log')}}</button>
-            {{-- TO DO : Add confirmation --}}
-        </form>
-        @endcan
-    </div>
-</div>
+<article class="log log--timeline">
+    <p class="log__text">
+        {{ $log->body }}
+    </p>
+    <footer class="log__footer">
+        <time datetime="{!! date('d/m/Y', strtotime($log->created_at)) !!}">{!! date('d/m/Y', strtotime($log->created_at)) !!}</time>
+        @include('partials.logs.actions')
+    </footer>
+</article>
