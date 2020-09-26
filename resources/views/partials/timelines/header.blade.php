54681a498422000bda711e9f78eb23f88d9ed0c1
Heymi <heymicodes@gmail.com>
2020-09-26 18:18:27 +0200

    Store part of timeline into separate files

---
+++ resources/views/partials/timelines/header.blade.php
@@ -0,0 +1,16 @@
+<header class="header-section header-section--timeline">
+    <h1 class="text-reveal">{{ $timeline->title }}</h1>
+    <div class="meta">
+        <address>by <a href="{{ route('profile.show', $user ) }}" rel="author" class="is-bold is-link">{{ $user->name }}</a></address>
+        <div>created on <time pubdate datetime="{!! date('d/m/Y', strtotime($timeline->created_at)) !!}">{!! date('d/m/Y', strtotime($timeline->created_at)) !!}</time> | updated on <time datetime="{!! date('d/m/Y', strtotime($timeline->updated_at)) !!}">{!! date('d/m/Y', strtotime($timeline->updated_at)) !!}</time>
+        </div>
+    </div>
+
+    @if($timeline->description)
+        <p>{{ $timeline->description }}</p>
+    @endif
+
+    @if($timeline->url_web)
+        <a class="is-link" href="{{ $timeline->url_web }}">30yearsofweb.site</a>
+    @endif
+</header>
