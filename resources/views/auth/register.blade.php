db147ed6dd9e00bcf34ee6a69138e17afce438d8
Heymi <heymicodes@gmail.com>
2020-09-26 13:15:09 +0200

    Replace bootstrap by custom css on register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<section class="{{Request::path()}}-section__form container">
+    @include('auth.auth-tabs')
+    <form method="POST" action="{{ route(Request::path()) }}" class="form">
+        @csrf
+        <div class="field @error('name') error @enderror">
+            <label for="name">{{ __('Name') }}</label>
+            <input id="name" type="text" name="name" value="{{ old('name') }}" placeholder="Amy" required autocomplete="name" autofocus>
+            @error('name')
+                <em role="alert">{{ $message }}</em>
+            @enderror
         </div>
-    </div>
-</div>
+
+        <div class="field @error('email') error @enderror">
+            <label for="email" >{{ __('E-Mail Address') }}</label>
+            <input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="mail@example.com" required autocomplete="email">
+            @error('email')
+                <em role="alert">{{ $message }}</em>
+            @enderror
+        </div>
+
+        <div class="field @error('password') error @enderror">
+            <label for="password">{{ __('Password') }}</label>
+            <span class="hint">{{ __('The password must contain at least 8 caracters.') }}</span>
+            <input id="password" type="password" name="password" required autocomplete="new-password">
+            @error('password')
+                <em role="alert">{{ $message }}</em>
+            @enderror
+        </div>
+
+        <div class="field">
+            <label for="password-confirm">{{ __('Confirm Password') }}</label>
+            <input id="password-confirm" type="password" name="password_confirmation" required autocomplete="new-password">
+        </div>
+
+        <div class="form__footer">
+            <button type="submit" class="btn btn--primary">
+                {{ __('Register') }}
+            </button>
+        </div>
+    </form>
+</section>
 @endsection
