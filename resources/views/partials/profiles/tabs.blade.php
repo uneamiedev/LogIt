5355cfa4701e7e8dee3c48d5859d70be273ffb7c
Heymi <heymicodes@gmail.com>
2020-09-27 14:09:53 +0200

    Update following tab

--- resources/views/partials/profiles/tabs.blade.php
+++ resources/views/partials/profiles/tabs.blade.php
@@ -2,7 +2,7 @@
     <ul>
         <li class="tabs__item @if(Route::currentRouteName() === 'timeline.index.public') current @endif"><a href="{{ route('timeline.index.public', $user) }}">Timelines</a></li>
         <li class="tabs__item @if(Route::currentRouteName() === 'profile.show') current @endif"><a href="{{ route('profile.show', $user) }}">Logs</a></li>
-        <li class="tabs__item"><a href="#">Following</a></li>
+        <li class="tabs__item @if(Route::currentRouteName() === 'profile.following') current @endif"><a href="{{ route('profile.following', $user) }}">Following</a></li>
         <li class="tabs__item"><a href="#">Followers</a></li>
     </ul>
 </nav>
