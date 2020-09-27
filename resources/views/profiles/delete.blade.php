dea844ed010c2e63ad5ce38edfbc99a8a68a87d7
Heymi <heymicodes@gmail.com>
2020-09-27 16:52:42 +0200

    Remove bootstrap from user deletion form

--- resources/views/profiles/delete.blade.php
+++ resources/views/profiles/delete.blade.php
@@ -1,9 +1,15 @@
-<div class="card mb-4">
+@extends('layouts.app')
+
+@section('content')
+<section class="containde">
+    <h1>{{ __('Delete your account') }}</h1>
+    <p>{{ __('Your profile, timelines and logs will be deleted from LogIt.') }}</p>
     <form method="POST" action="{{ route('profile.destroy', ['user' => auth()->user()]) }}">
         @csrf
-        <button type="submit" class="btn btn-danger btn-lg">
+        <button type="submit" class="btn btn--danger">
             @method('DELETE')
             {{ __('Delete account')}}
         </button>
     </form>
-</div>
+</section>
+@endsection
