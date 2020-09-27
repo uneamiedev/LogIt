128ccca48997830b4b8ac522b8ff08cd0faf6a2f
Heymi <heymicodes@gmail.com>
2020-09-27 12:01:17 +0200

    Update profile's tab timelines

--- resources/views/partials/profiles/tabs.blade.php
+++ resources/views/partials/profiles/tabs.blade.php
@@ -1,6 +1,6 @@
 <nav class="tabs">
     <ul>
-        <li class="tabs__item"><a href="#">Timelines</a></li>
+        <li class="tabs__item @if(Route::currentRouteName() === 'timeline.index.public') current @endif"><a href="{{ route('timeline.index.public', $user) }}">Timelines</a></li>
         <li class="tabs__item @if(Route::currentRouteName() === 'profile.show') current @endif"><a href="{{ route('profile.show', $user) }}">Logs</a></li>
         <li class="tabs__item"><a href="#">Following</a></li>
         <li class="tabs__item"><a href="#">Followers</a></li>
